7fc34f80e424704d5dee78c7660db298e99c380a
wooker <zakhar.semenov@nu.edu.kz>
2024-05-10 17:05:11 +0500

    Claim resource via PeripheralRef of esp_hal

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,4 +1,5 @@
 use crate::multiplexer::Multiplexer;
+use esp_hal::prelude::*;
 
 /// Main communication layer between HALs and LibOSes
 pub struct Kernel {
@@ -8,11 +9,13 @@ pub struct Kernel {
 // static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 // static mut HEAP: core::mem::MaybeUninit<[u8; 1024]> = core::mem::MaybeUninit::uninit();
 
-use esp_hal::peripherals::*;
+use esp_hal::peripherals::Peripherals as HALPeripherals;
 
 impl Kernel {
     pub fn new() -> Self {
-        let multiplexer = Multiplexer::new();
+        let peripherals = HALPeripherals::take();
+        // let system = peripherals.SYSTEM.split();
+        let multiplexer = Multiplexer::new(&mut Some(peripherals));
         Self { multiplexer }
     }
 }
--- src/multiplexer.rs
+++ src/multiplexer.rs
@@ -3,46 +3,11 @@ use esp_hal::{
     peripherals::{Peripherals, AES, SYSTEM},
 };
 
-pub enum AvailablePeripherals {
-    SYSTEM,
-    AES,
+pub struct Resource<'res, T: Peripheral<P = T>> {
+    inner: PeripheralRef<'res, T>,
 }
-
-pub enum Resource {
-    System(SYSTEM),
-    Aes(AES),
-}
-impl Resource {
-    fn system(system: SYSTEM) -> Self {
-        Resource::System(system)
-    }
-
-    fn aes(aes: AES) -> Self {
-        Resource::Aes(aes)
-    }
-
-    pub fn inner_system(self) -> Option<SYSTEM> {
-        if let Resource::System(system) = self {
-            Some(system)
-        } else {
-            None
-        }
-    }
-
-    pub fn inner_aes(self) -> Option<AES> {
-        if let Resource::Aes(aes) = self {
-            Some(aes)
-        } else {
-            None
-        }
-    }
-}
-
-pub struct Res<T: Peripheral<P = T>> {
-    inner: T,
-}
-impl Res<SYSTEM> {
-    fn inner(self, p: impl Peripheral<P = SYSTEM>) -> SYSTEM {
+impl<'res, T: Peripheral<P = T>> Resource<'res, T> {
+    pub fn inner(self) -> PeripheralRef<'res, T> {
         self.inner
     }
 }
@@ -52,20 +17,43 @@ pub struct Multiplexer {
 }
 
 impl Multiplexer {
-    pub fn new() -> Self {
+    pub fn new(peripherals: &mut Option<Peripherals>) -> Self {
         Multiplexer {
-            peripherals: Peripherals::take(),
+            peripherals: Option::take(peripherals).unwrap(),
         }
     }
+}
 
-    pub fn claim<'a>(&'a mut self, peripheral: AvailablePeripherals) -> Resource {
-        match peripheral {
-            AvailablePeripherals::SYSTEM => unsafe {
-                Resource::System(self.peripherals.SYSTEM.clone_unchecked())
-            },
-            AvailablePeripherals::AES => unsafe {
-                Resource::Aes(self.peripherals.AES.clone_unchecked())
-            },
-        }
+impl Multiplexer {
+    pub fn system<T: Peripheral<P = SYSTEM>>(self) -> PeripheralRef<'static, SYSTEM> {
+        self.peripherals.SYSTEM.into_ref()
+    }
+
+    pub fn aes<T: Peripheral<P = AES>>(self) -> PeripheralRef<'static, AES> {
+        self.peripherals.AES.into_ref()
+    }
+}
+
+pub trait Claim {
+    type Item;
+
+    fn claim(self) -> PeripheralRef<'static, Self::Item>
+    where
+        <Self as Claim>::Item: Peripheral<P = Self::Item>;
+}
+
+impl Claim for Resource<'static, AES> {
+    type Item = AES;
+
+    fn claim(self) -> PeripheralRef<'static, AES> {
+        self.inner
+    }
+}
+
+impl Claim for Resource<'static, SYSTEM> {
+    type Item = SYSTEM;
+
+    fn claim(self) -> PeripheralRef<'static, SYSTEM> {
+        self.inner
     }
 }
