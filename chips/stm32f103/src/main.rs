da259a4f02f5a9881b1ad0f8280e29c8b1c04ba6
wooker <zakhar.semyonov@gmail.com>
2024-09-26 16:22:40 +0500

    chips/stm32f103: use embassy

--- chips/stm32f103/src/main.rs
+++ chips/stm32f103/src/main.rs
@@ -1,32 +1,26 @@
 #![no_std]
 #![no_main]
 
-use core::arch::asm;
-use orbit::{entry, CPeripherals, FlashExt, GpioExt, Kernel, Peripherals, RccExt, Timer, *};
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
-#[entry]
-fn main() -> ! {
-    // let kernel = Kernel::new();
-    // hprintln!("Kernel initialized.");
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
 
-    let dp = Peripherals::take().unwrap();
-    let cp = CPeripherals::take().unwrap();
-
-    let mut flash = dp.FLASH.constrain();
-    let rcc = dp.RCC.constrain();
-
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut gpioc = dp.GPIOC.split();
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
 
     loop {
-        timer.wait().unwrap();
+        info!("high");
         led.set_high();
-        timer.wait().unwrap();
+        Timer::after_millis(1000).await;
+
+        info!("low");
         led.set_low();
+        Timer::after_millis(300).await;
     }
 }
