e0570123909daa343db9d624be351b5d14207a31
wooker <zakhar.semyonov@gmail.com>
2025-10-15 17:36:05 +0500

    wip: errors in kernel-app communications

--- orbit-apps/reade/src/lib.rs
+++ orbit-apps/reade/src/lib.rs
@@ -9,7 +9,7 @@ use eink_lib::{Config as EinkConfig, Direction, Eink as LibEink, Position, Size}
 use font_8x10::{self, ASCII};
 
 app_heap!(0);
-app_stack!(32);
+app_stack!(128);
 
 const DC_PIN: u8 = 1;
 const BUSY_PIN: u8 = 6;
@@ -69,16 +69,29 @@ impl Reade {
                         }
                     }
                     let config = EinkConfig {
+<<<<<<< HEAD
                         pos: Position {
                             x: math::add_t::<usize>(0, 0),
                             y: 0,
                         },
+=======
+                        pos: Position { x: 0, y: 0 },
+>>>>>>> 73cce2c (wip: errors in kernel-app communications)
                         dir: Direction::XuYiXi,
                         size: Size {
                             width: 200,
                             height: 200,
                         },
                     };
+<<<<<<< HEAD
+=======
+                    let mut gpioa = Self::claim_peripheral_gpioa();
+                    let mut gpioa = Claimed::new(&mut gpioa);
+                    let mut spi1 = Self::claim_peripheral_spi1();
+                    let mut spi1 = Claimed::new(&mut spi1);
+                    let bus = LibSpi::new(&mut spi1, Config::default());
+                    let mut eink: LibEink<DC_PIN, BUSY_PIN> = LibEink::new(bus, &mut gpioa);
+>>>>>>> 73cce2c (wip: errors in kernel-app communications)
                     eink.display(config, &self.frame.buf, false);
                     self.letters.flush();
                     self.frame.flush();
@@ -87,12 +100,22 @@ impl Reade {
                 }
                 _ => {
                     let chars = arg.chars();
+<<<<<<< HEAD
                     chars.clone().for_each(|b| self.letters.push(b as u8));
                     // self._buf.buf[..25]
                     //     .iter()
                     //     .for_each(|b| self.letters.push(*b));
                     // self._buf.flush();
                     Output([chars.count() as u8])
+=======
+                    chars.for_each(|b| self.letters.push(b as u8));
+
+                    // self._buf.buf[..25]
+                    //     .iter()
+                    //     .for_each(|b| self.letters.push(*b));
+                    self._buf.flush();
+                    Output([2])
+>>>>>>> 73cce2c (wip: errors in kernel-app communications)
                 }
             }
         } else {
